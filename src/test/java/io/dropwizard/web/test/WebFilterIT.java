9c7a23ca072de1ce68be85b8cf67ca764e969c2c
Jochen Schalanda <jochen@schalanda.name>
2021-06-27 16:57:25 +0200

    Support applying `WebBundle` multiple times
    In order to support multiple instances of `WebBundle` with different configurations, the Servlet Filter name has to be unique per bundle.
    
    Closes #32

--- src/test/java/io/dropwizard/web/test/WebFilterIT.java
+++ src/test/java/io/dropwizard/web/test/WebFilterIT.java
@@ -7,16 +7,14 @@ import org.junit.ClassRule;
 import org.junit.Test;
 
 import javax.ws.rs.client.Client;
-import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.core.Response;
 
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
-import static org.junit.Assert.assertThat;
 
 public class WebFilterIT {
     private static final String CONFIG_DIR = "src/test/resources/conf";
     private static final String CONFIG_PATH = CONFIG_DIR + "/config.yml";
-    private static final String HOST = "localhost";
 
     @ClassRule
     public static final DropwizardAppRule<TestConfig> APP = new DropwizardAppRule<>(TestApp.class, CONFIG_PATH);
@@ -26,8 +24,8 @@ public class WebFilterIT {
 
     @Before
     public void setUp() throws Exception {
-        client = ClientBuilder.newClient();
-        hostUrl = "http://" + HOST + ":" + APP.getLocalPort();
+        client = APP.client();
+        hostUrl = "http://localhost:" + APP.getLocalPort();
     }
 
     @After
@@ -38,19 +36,24 @@ public class WebFilterIT {
     @Test
     public void testHeaders() {
         // given
-        String url = hostUrl + "/test";
+        String uri1 = hostUrl + "/test/one";
+        String uri2 = hostUrl + "/test/two";
 
         // when
-        Response response = client.target(url).request().get();
+        Response response1 = client.target(uri1).request().get();
+        Response response2 = client.target(uri2).request().get();
 
         // then
-        assertThat(response.readEntity(String.class), is("test response"));
-        assertThat(response.getHeaderString("Strict-Transport-Security"), is("max-age=31536000; includeSubDomains"));
-        assertThat(response.getHeaderString("X-Frame-Options"), is("DENY"));
-        assertThat(response.getHeaderString("X-Content-Type-Options"), is("nosniff"));
-        assertThat(response.getHeaderString("X-XSS-Protection"), is("1; mode=block"));
-        assertThat(response.getHeaderString("Content-Security-Policy"), is("test csp policy"));
-        assertThat(response.getHeaderString("X-Custom-Header-1"), is("custom value 1"));
-        assertThat(response.getHeaderString("X-Custom-Header-2"), is("custom value 2"));
+        assertThat(response1.readEntity(String.class), is("test response 1"));
+        assertThat(response1.getHeaderString("Strict-Transport-Security"), is("max-age=31536000; includeSubDomains"));
+        assertThat(response1.getHeaderString("X-Frame-Options"), is("DENY"));
+        assertThat(response1.getHeaderString("X-Content-Type-Options"), is("nosniff"));
+        assertThat(response1.getHeaderString("X-XSS-Protection"), is("1; mode=block"));
+        assertThat(response1.getHeaderString("Content-Security-Policy"), is("test csp policy"));
+        assertThat(response1.getHeaderString("X-Custom-Header-1"), is("custom value 1"));
+        assertThat(response1.getHeaderString("X-Custom-Header-2"), is("custom value 2"));
+        assertThat(response2.readEntity(String.class), is("test response 2"));
+        assertThat(response2.getHeaderString("X-Second-Custom-Header"), is("custom value"));
     }
+
 }
