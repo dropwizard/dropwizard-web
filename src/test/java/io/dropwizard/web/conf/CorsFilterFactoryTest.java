4ceb47be1d2e83997cc0ffedcda7b5410d24c14d
jwyke <jens.wyke@paypal.com>
2025-10-20 14:12:03 +0200

    Updated CorsFilterFactoryTest to use CrossOriginHandler instead of CrossOriginFilter

--- src/test/java/io/dropwizard/web/conf/CorsFilterFactoryTest.java
+++ src/test/java/io/dropwizard/web/conf/CorsFilterFactoryTest.java
@@ -1,10 +1,11 @@
 package io.dropwizard.web.conf;
 
 import com.google.common.collect.ImmutableList;
-import io.dropwizard.jetty.setup.ServletEnvironment;
+import io.dropwizard.jetty.MutableServletContextHandler;
 import io.dropwizard.core.setup.Environment;
-import org.eclipse.jetty.ee10.servlets.CrossOriginFilter;
-import org.hamcrest.CoreMatchers;
+import java.util.Set;
+import org.eclipse.jetty.server.Handler;
+import org.eclipse.jetty.server.handler.CrossOriginHandler;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.ArgumentCaptor;
@@ -12,23 +13,10 @@ import org.mockito.Captor;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
-import java.util.Collections;
-import java.util.EnumSet;
-import java.util.Map;
-
-import jakarta.servlet.Filter;
-import jakarta.servlet.FilterRegistration;
-
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.aMapWithSize;
-import static org.hamcrest.Matchers.hasEntry;
 import static org.hamcrest.Matchers.is;
-
+import static org.hamcrest.Matchers.notNullValue;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyBoolean;
-import static org.mockito.ArgumentMatchers.anyMap;
-import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
@@ -37,17 +25,9 @@ public class CorsFilterFactoryTest {
     @Mock
     Environment env;
     @Mock
-    ServletEnvironment servlets;
-    @Mock
-    FilterRegistration.Dynamic registration;
+    MutableServletContextHandler contextHandler;
     @Captor
-    ArgumentCaptor<String> filterNameCaptor;
-    @Captor
-    ArgumentCaptor<Class<? extends Filter>> filterClassCaptor;
-    @Captor
-    ArgumentCaptor<String> urlPatternCaptor;
-    @Captor
-    ArgumentCaptor<Map<String, String>> initParamsCaptor;
+    ArgumentCaptor<Handler.Singleton> handlerCaptor;
 
     @BeforeEach
     public void setUp() throws Exception {
@@ -55,12 +35,10 @@ public class CorsFilterFactoryTest {
     }
 
     @Test
-    public void configureFilter() {
+    public void configureHandler() {
         // given
-        when(env.servlets()).thenReturn(servlets);
-        when(servlets.addFilter(anyString(), any(Class.class))).thenReturn(registration);
-        doNothing().when(registration).addMappingForUrlPatterns(any(EnumSet.class), anyBoolean(), anyString());
-        when(registration.setInitParameters(anyMap())).thenReturn(Collections.emptySet());
+        when(env.getApplicationContext()).thenReturn(contextHandler);
+        doNothing().when(contextHandler).insertHandler(any(Handler.Singleton.class));
         String urlPattern = "/example/*";
         CorsFilterFactory factory = new CorsFilterFactory();
         factory.setAllowedOrigins(ImmutableList.of("example.com", "foo.com"));
@@ -69,13 +47,13 @@ public class CorsFilterFactoryTest {
         factory.build(env, urlPattern);
 
         // then
-        verify(servlets).addFilter(filterNameCaptor.capture(), filterClassCaptor.capture());
-        verify(registration).addMappingForUrlPatterns(any(), eq(true), urlPatternCaptor.capture());
-        verify(registration).setInitParameters(initParamsCaptor.capture());
-        assertThat(filterNameCaptor.getValue(), is("cross-origin-filter"));
-        assertThat(filterClassCaptor.getValue(), is(CoreMatchers.<Class<?>>equalTo(CrossOriginFilter.class)));
-        assertThat(urlPatternCaptor.getValue(), is(urlPattern));
-        assertThat(initParamsCaptor.getValue(), aMapWithSize(1));
-        assertThat(initParamsCaptor.getValue(), hasEntry(CrossOriginFilter.ALLOWED_ORIGINS_PARAM, "example.com,foo.com"));
+        verify(contextHandler).insertHandler(handlerCaptor.capture());
+        assertThat(handlerCaptor.getValue(), is(notNullValue()));
+        assertThat(handlerCaptor.getValue() instanceof CrossOriginHandler, is(true));
+        assertThat(((CrossOriginHandler) handlerCaptor.getValue())
+                .getAllowedOriginPatterns()
+                , is(Set.of("example.com", "foo.com")));
+
+
     }
 }
